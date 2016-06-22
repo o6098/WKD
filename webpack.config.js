8b2126e8c3eb6503e657418e63d4c392ebe51528
o6098 <oooo6098@gmail.com>
2016-06-22 11:17:31 +0800

    webbbbbbbbbbpackkkkkage

--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,7 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel',
-      query: {
-        presets: ['es2015', 'react']
-      }
+      
     }]
   }
 };
