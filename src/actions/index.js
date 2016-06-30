077a1ca5656b6335b99864d98109cbea2efacef1
o6098 <oooo6098@gmail.com>
2016-06-30 21:28:35 +0800

    heroku10

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_POST = 'FETCH_POST';
 export const DELETE_POST = 'DELETE_POST';
 
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=854712';
 
 export function fetchPost(id){
